2963e026d6118985e71c0de9822007888cab0768
Alex Ford <alexford@frontside.io>
2017-07-06 15:00:01 -0600

    Revert PR #52 Don't import Bugsnag in test build

--- index.js
+++ index.js
@@ -23,40 +23,21 @@ module.exports = {
     }
   },
 
-  config: function(env, config) {
-    const { bugsnag } = config;
-    const { releaseStage = env } = bugsnag;
-    this._includeBugsnag = this.isDevelopingAddon() || releaseStage !== 'test';
-
+  config: function() {
     return {
       bugsnag: readEnvironmentConfig(process.env)
     };
   },
 
-  treeForAddon: function() {
-    if (this._includeBugsnag) {
-      return this._super.treeForAddon.apply(this, arguments);
-    }
-  },
-
-  treeForApp: function() {
-    if (this._includeBugsnag) {
-      return this._super.treeForApp.apply(this, arguments);
-    }
-  },
-
   included: function(app) {
     this._super.included.apply(this, arguments);
 
-    if (this._includeBugsnag) {
-      app.import('vendor/bugsnag-js/bugsnag.js');
-
-      app.import('vendor/bugsnag/shim.js', {
-        type: 'vendor',
-        exports: {
-          bugsnag: ['default']
-        }
-      });
-    }
+    app.import('vendor/bugsnag-js/bugsnag.js');
+    app.import('vendor/bugsnag/shim.js', {
+      type: 'vendor',
+      exports: {
+        bugsnag: ['default']
+      }
+    });
   }
 };
